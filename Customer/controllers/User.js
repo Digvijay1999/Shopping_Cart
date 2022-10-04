a102a4a7f8d4cfc56767ab28a24443b033e6c66d
Digvijay <digvijayyamagekar@gmail.com>
2022-10-04 22:38:40 +0530

    changed login functionality

--- Customer/controllers/User.js
+++ Customer/controllers/User.js
@@ -1,34 +1,45 @@
-const {
-    createCustomer,
-    signToken,
-    getId,
-    checkUsername,
-    CheckPassword
-} = require('../services/User')
+const customerServices = require('../services/User')
+const Auth = require("../services/Auth")
 
 const signup = async (req, res) => {
-    await createCustomer(req);
-    let customerId = await getId(req.body.username);
-    let token = signToken(req.body, customerId);
+
+    await customerServices.createCustomer(req);
+    let customerId = await customerServices.getId(req.body.username);
+    let token = Auth.signToken(req.body, customerId);
+
     res.cookie('AuthToken', token, { httpOnly: true })
-    return;
+    res.end("signed up Token is :" + token);
 }
 
-const login = (req, res) => {
-    let user = checkUsername(req);
-    if (!user === undefined) {
-        let pass = CheckPassword(req)
-        if (!pass === undefined) {
-            let customerId = getId(req.body.username);
-            let token = signToken(req.body, customerId);
+const login = async (req, res) => {
+
+    let user = await customerServices.checkUsername(req);
+
+    if (user != undefined) {
+        console.log(user + " user exists ");
+
+        let pass = await customerServices.CheckPassword(req)
+        if (pass == true) {
+            console.log("password matched");
+            console.log("user verified returning token");
+            let customerId = await customerServices.getId(req.body.username);
+            let token = Auth.signToken(req.body, customerId);
             res.cookie('AuthToken', token, { httpOnly: true })
+            res.end(token)
+        } else {
+            res.end("username or password is incorrect")
             return;
         }
+    } else {
+        res.end("username or password is incorrect")
+        return;
     }
 }
 
-const profile = (req, res) => {
-
+const profile = async (req, res) => {
+    let data = await customerServices.getUserDetails(req.cookies.AuthToken);
+    res.json(data);
+    res.end();
 }
 
 module.exports = {
