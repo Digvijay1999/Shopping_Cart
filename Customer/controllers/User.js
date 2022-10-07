a69bf981076426c307a869c3280538f828ba76d7
Digvijay <digvijayyamagekar@gmail.com>
2022-10-07 18:00:39 +0530

    formatting

--- Customer/controllers/User.js
+++ Customer/controllers/User.js
@@ -14,23 +14,21 @@ const signup = async (req, res) => {
 const login = async (req, res) => {
 
     let user = await customerServices.checkUsername(req);
-
     if (user != undefined) {
-        console.log(user + " user exists ");
-
         let pass = await customerServices.CheckPassword(req)
         if (pass == true) {
-            console.log("password matched");
-            console.log("user verified returning token");
             let customerId = await customerServices.getId(req.body.username);
             let token = Auth.signToken(req.body, customerId);
+
             res.cookie('AuthToken', token, { httpOnly: true })
             res.end(token)
         } else {
+
             res.end("username or password is incorrect")
             return;
         }
     } else {
+
         res.end("username or password is incorrect")
         return;
     }
--- Customer/routers/User.js
+++ Customer/routers/User.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const router = express.Router()
-const cookieParser = require("cookie-parser")
-
 
 const customerControllers = require('../controllers/User')
 
